fe7e2f4fd4183b78e4b776e2c04367306bae7946
Kaspars <hi@kaspars.net>
2015-07-22 17:41:29 -0700

    Always update the PHP source

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ sudo mkdir /usr/local/php7
 git clone https://github.com/php/php-src.git
 cd php-src
 git checkout PHP-7.0.0
+git pull
 ./buildconf --force
 
 CONFIGURE_STRING="--prefix=/usr/local/php7 \
