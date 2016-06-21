7de86c392869eee36cc4183e83bf41d2fee025d6
Kaspars Dambis <hi@kaspars.net>
2016-06-21 19:16:53 +0300

    Don't restart by default

--- extensions/imagick-install.sh
+++ extensions/imagick-install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # Add config files
-echo "extension=imagick.so" >> /usr/local/php7/etc/conf.d/modules.ini
-
-service php7-fpm restart
+sudo echo "extension=imagick.so" >> /usr/local/php7/etc/conf.d/modules.ini
