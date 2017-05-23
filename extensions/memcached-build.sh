c06c28f253f9975153a5f3f6a702529a4ff98b8e
Kaspars Dambis <hi@kaspars.net>
2017-05-23 16:41:14 +0300

    Always fetch the latest tagged release

--- extensions/memcached-build.sh
+++ extensions/memcached-build.sh
@@ -9,8 +9,15 @@ sudo apt-get install -y \
 
 git clone https://github.com/php-memcached-dev/php-memcached
 cd php-memcached
-git checkout php7
-git pull
+
+# Fetch the latest changes
+git fetch --tags --prune
+
+# Get the latest tag
+TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+git checkout -f tags/$TAG
+git reset --hard
 
 /usr/local/php7/bin/phpize
 ./configure --with-php-config=/usr/local/php7/bin/php-config
