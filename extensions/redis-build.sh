5ff61a6d82a028a4e753910e0d5b965b426be066
Kaspars Dambis <hi@kaspars.net>
2017-05-23 16:47:21 +0300

    Fetch the latest release tag of Redis

--- extensions/redis-build.sh
+++ extensions/redis-build.sh
@@ -8,8 +8,15 @@ sudo apt-get install -y \
 
 git clone https://github.com/phpredis/phpredis.git
 cd phpredis
-git checkout php7
-git pull
+
+# Fetch the latest changes
+git fetch --tags --prune
+
+# Get the latest tag
+TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+git checkout -f tags/$TAG
+git reset --hard
 
 /usr/local/php7/bin/phpize
 ./configure --with-php-config=/usr/local/php7/bin/php-config
