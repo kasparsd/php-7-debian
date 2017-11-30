bf3a776cbb853b095c476c964aad5a60345ae151
Kaspars Dambis <hi@kaspars.net>
2017-11-30 17:59:36 +0200

    7.2.0 is out!

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ sudo mkdir /usr/local/php7
 git clone https://github.com/php/php-src.git
 cd php-src
 git fetch --tags --prune
-git checkout tags/php-7.1.12
+git checkout tags/php-7.2.0
 ./buildconf --force
 
 CONFIGURE_STRING="--prefix=/usr/local/php7 \
