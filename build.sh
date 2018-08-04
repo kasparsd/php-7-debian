1743452e3e757bc256e5d3b9a0ae91c65561d3ed
Kaspars Dambis <hi@kaspars.net>
2018-08-04 14:14:05 +0300

    Use the auto search instead
    —with-mysql-sock is ignored now that the legacy mysql has been removed, see https://github.com/php/php-src/pull/361

--- build.sh
+++ build.sh
@@ -50,7 +50,6 @@ CONFIGURE_STRING="--prefix=/usr/local/php7 \
                   --enable-mbstring \
                   --with-mhash \
                   --enable-mysqlnd \
-                  --with-mysql-sock=/var/run/mysqld/mysqld.sock \
                   --with-mysqli \
                   --with-pdo-mysql \
                   --with-openssl \
