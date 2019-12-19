d3d4632a94a96f8002f6f9f245709cf0eafe53bd
Kaspars Dambis <hi@kaspars.net>
2019-12-19 22:28:17 +0200

    Bump to PHP 7.4.1

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ sudo mkdir /usr/local/php7
 git clone https://github.com/php/php-src.git
 cd php-src
 git fetch --tags --prune
-git checkout tags/php-7.4.0
+git checkout tags/php-7.4.1
 ./buildconf --force
 
 CONFIGURE_STRING="--prefix=/usr/local/php7 \
